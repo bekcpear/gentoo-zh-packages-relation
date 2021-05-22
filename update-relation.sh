a18c87507fceceafc06d1b54eeb6dd683f3ca25d
bekcpear <i@bitbili.net>
2021-05-22 21:44:43 +0800

    change date format

--- update-relation.sh
+++ update-relation.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-ctime="$(TZ=Asia/Shanghai date '+%Y-%m-%d %H:%M:%S')"
+ctime="$(TZ=Asia/Shanghai date '+%Y-%m-%d %H:%M:%S %Z')"
 
 pushd gentoo-zh
 ../relation-of-ebuilds-in-overlay-r1-for-gentoo-zh.sh "-t${ctime}" >../relation.md
@@ -15,7 +15,7 @@ if [[ ${_gitdiff%% } != '@@ -1,6 +1,6 @@' ]]; then
   git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
   git config --local user.name "github-actions[bot]"
   git add .
-  git commit -m "${ctime}"
+  git commit -m "${ctime} (updated)"
   echo "::set-output name=state::changed"
 else
   echo "::set-output name=state::unchanged"
